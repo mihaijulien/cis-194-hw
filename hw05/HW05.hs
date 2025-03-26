c2059ec88df7eac8f09ec389f4fe1cd69ab3c565
mihaijulien <mihaijulien@gmail.com>
2025-03-26 14:32:39 +0200

    start hw05 add files

---
+++ hw05/HW05.hs
@@ -0,0 +1,97 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+module HW05 where
+
+import Data.ByteString.Lazy (ByteString)
+import Data.Map.Strict (Map)
+import System.Environment (getArgs)
+
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map.Strict as Map
+
+import Data.Bits (xor)
+import Parser
+
+-- Exercise 1 -----------------------------------------
+
+-- FilePath is just a synonym for String
+-- https://hackage.haskell.org/package/bytestring-0.12.2.0/docs/Data-ByteString-Lazy.html#g:25
+
+getSecret :: FilePath -> FilePath -> IO ByteString
+getSecret modified original = 
+  do 
+    modf <- BS.readFile modified
+    origf <- BS.readFile original
+    let diff = BS.filter  (/=0) . BS.pack $ BS.zipWith xor modf origf
+    return diff
+
+-- Exercise 2 -----------------------------------------
+
+decryptWithKey :: ByteString -> FilePath -> IO ()
+decryptWithKey = undefined
+
+-- Exercise 3 -----------------------------------------
+
+parseFile :: FromJSON a => FilePath -> IO (Maybe a)
+parseFile = undefined
+
+-- Exercise 4 -----------------------------------------
+
+getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
+getBadTs = undefined
+
+-- Exercise 5 -----------------------------------------
+
+getFlow :: [Transaction] -> Map String Integer
+getFlow = undefined
+
+-- Exercise 6 -----------------------------------------
+
+getCriminal :: Map String Integer -> String
+getCriminal = undefined
+
+-- Exercise 7 -----------------------------------------
+
+undoTs :: Map String Integer -> [TId] -> [Transaction]
+undoTs = undefined
+
+-- Exercise 8 -----------------------------------------
+
+writeJSON :: ToJSON a => FilePath -> a -> IO ()
+writeJSON = undefined
+
+-- Exercise 9 -----------------------------------------
+
+doEverything :: FilePath -> FilePath -> FilePath -> FilePath -> FilePath
+             -> FilePath -> IO String
+doEverything dog1 dog2 trans vict fids out = do
+  key <- getSecret dog1 dog2
+  decryptWithKey key vict
+  mts <- getBadTs vict trans
+  case mts of
+    Nothing -> error "No Transactions"
+    Just ts -> do
+      mids <- parseFile fids
+      case mids of
+        Nothing  -> error "No ids"
+        Just ids -> do
+          let flow = getFlow ts       
+          writeJSON out (undoTs flow ids)
+          return (getCriminal flow)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  crim <- 
+    case args of
+      dog1:dog2:trans:vict:ids:out:_ ->
+          doEverything dog1 dog2 trans vict ids out
+      _ -> doEverything "dog-original.jpg"
+                        "dog.jpg"
+                        "transactions.json"
+                        "victims.json"
+                        "new-ids.json"
+                        "new-transactions.json"
+  putStrLn crim
+
+
